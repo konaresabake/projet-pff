685011799a8d7a2de453ad345fb65e8c804f2d59
sabake.konare <sabakekonare55@gmail.com>
2025-08-22 20:52:43 +0000

    ajout des app au setting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'lot',
+    'tache',
+    'rapport',
+    'chantier',
+    'budget',
+    'ressource',
+    'fournisseur',
+    'alert',
+    'utilisateur',
+    'projet',
     
 ]
 
